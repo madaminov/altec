f259ebbd688c81d633ff5a81406a72608c3c2ab1
Dmitriy <madaminov.d@gmail.com>
2025-03-14 10:30:08 +0500

    add filex

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ document
   .addEventListener('click', (event) => {
     mobile_menu.classList.remove('open');
   });
+formControl = document.querySelectorAll(
+  '.form-group-label-transform .form-control'
+);
+formControl.forEach((box) => {
+  box.addEventListener('focus', function (event) {
+    form_group = event.target.closest('.form-group-label-transform');
+    form_group.classList.add('form-group-label-top');
+  });
+  box.addEventListener('blur', function (event) {
+    form_group = event.target.closest('.form-group-label-transform');
+    if (event.target.value.length > 0) {
+      form_group.classList.add('form-group-label-top');
+    } else {
+      form_group.classList.remove('form-group-label-top');
+    }
+  });
+});
